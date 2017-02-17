dc1e6a3858a6a8131978da1087157081297d7440
rafek <rafal.legiedz@gmail.com>
2017-02-17 14:00:23 +0000

    Azure IoT connection and listening for device method calls.

---
+++ app.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+const Client = require('azure-iot-device').Client
+const ConnectionString = require('azure-iot-device').ConnectionString
+const Protocol = require('azure-iot-device-mqtt').Mqtt
+
+const config = require('./config.json')
+const connectionString = ConnectionString.parse(config.device_connection_string)
+const deviceId = connectionString.DeviceId
+const client = Client.fromConnectionString(config.device_connection_string, Protocol)
+
+if (connectionString.x509) {
+  const x509Path = path.join(__dirname, 'device-x509')
+
+  const options = {
+    cert: fs.readFileSync(path.join(x509Path, deviceId + '-cert.pem')).toString(),
+    key: fs.readFileSync(path.join(x509Path, deviceId + '-key.pem')).toString()
+  }
+
+  client.setOptions(options)
+
+  console.log('[Device (' + deviceId + ')] Using X.509 client certificate authentication.')
+}
+
+function onOpenDoorCommand() {
+  // TODO: implement
+  console.log('Open Door command has been invoked.')
+}
+
+client.open(err => {
+  if (err) {
+    console.error('[Device (' + deviceId + ')] Could not connect: ' + err)
+  } else {
+    console.log('[Device (' + deviceId + ')] Client connected.')
+    
+    client.onDeviceMethod('open-door', onOpenDoorCommand) 
+ }
+})
